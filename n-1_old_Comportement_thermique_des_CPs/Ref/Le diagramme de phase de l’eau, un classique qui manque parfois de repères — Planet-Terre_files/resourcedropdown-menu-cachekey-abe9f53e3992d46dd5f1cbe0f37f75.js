8c7b32d8fa3e06cbf48db5b7eb9b421dd2669b49
jfolivie <jean-francois.olivieri@universite-paris-saclay.fr>
2026-08-06 14:35:39 +0200

    Standardisation du dépôt : nettoyage.

--- n-1_old_Comportement_thermique_des_CPs/Ref/Le diagramme de phase de l’eau, un classique qui manque parfois de repères — Planet-Terre_files/resourcedropdown-menu-cachekey-abe9f53e3992d46dd5f1cbe0f37f75.js
+++
@@ -1,4 +0,0 @@
-
-/* - ++resource++dropdown-menu.js - */
-// https://planet-terre.ens-lyon.fr/portal_javascripts/++resource++dropdown-menu.js?original=1
-jQuery(function($){$('#portal-globalnav .noClick').click(function(e){e.preventDefault()})});
--- n-1_old_Comportement_thermique_des_CPs/Ref/uc_DefPolymorphisme_files/skin.js
+++
@@ -1,8 +0,0 @@
-// Skin specific Javascript
-if ("scImgMgr" in window) scImgMgr.fOverAlpha=.9;
-
-// Mathjax config
-if ("mathjaxMgr" in window){
-	mathjaxMgr.fConfig_extentions='["tex2jax.js","mml2jax.js","MathML/content-mathml.js","MathMenu.js","MathZoom.js","fast-preview.js","AssistiveMML.js",]';
-	mathjaxMgr.fConfig_TeX='{extensions:["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js","cancel.js","color.js","color.js","mhchem.js"]}';
-}
